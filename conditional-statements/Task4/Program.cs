b8ffb1dcd9e6a68c9457b366cdcce109f457de7c
MiPo91 <k4supur3@gmail.com>
2017-09-12 13:56:18 +0300

    Finished Task 4

---
+++ conditional-statements/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user
+            Console.WriteLine("Please enter 3 different number");
+
+            
+            int[] numberList = new int[3];
+            
+            // Saving user inputs to Array
+            for(int i = 0; i < 3;i++)
+            {
+                string userInput;
+                userInput = Console.ReadLine();
+                
+                int.TryParse(userInput, out numberList[i]);
+            }
+            
+            // Sorting numbers
+            Array.Sort(numberList);
+
+            // Writing results to console
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
