b3455cfe56314ec388997a1f4f4c18b9f1f169b7
MiPo91 <k4supur3@gmail.com>
2017-09-13 12:28:10 +0300

    Finished Task 4

---
+++ functions/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give 10 positive Integers");
+
+            int[] functionResults = userResults(); // getting function results
+
+            Console.WriteLine("Your inputted numbers are:");
+            for(int i = 0; i < 10; i++) { // printing array values to user
+                Console.Write(functionResults[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+
+        public static int[] userResults()
+        {
+            int[] numberList = new int[10];
+            int i = 0;
+
+            while(i  < 10) { // looping until 10 positive numbers inputted
+
+                string userInput = Console.ReadLine();
+                int usersNumber;
+                int.TryParse(userInput, out usersNumber);
+
+                if(usersNumber > 0) {
+                    numberList[i] = usersNumber;
+                    i++;
+                } else {
+                    Console.WriteLine("Wrong input, give positive number");
+                }            
+            }
+
+            return numberList;
+        }
+    }
+}
