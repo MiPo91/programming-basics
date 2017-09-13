528feb7db31c7b56804c7b0a1d506d2d6a34719a
MiPo91 <k4supur3@gmail.com>
2017-09-13 09:03:42 +0300

    Finished Loops Task 1

---
+++ Loops/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give number!");
+
+            string numberGiven = Console.ReadLine();
+            int convertedNumber;
+            int.TryParse(numberGiven, out convertedNumber);
+            int calculatedResult = 1;
+
+            if(convertedNumber > 0)
+            {
+                for(int i = 1; i <= convertedNumber; i++)
+                {
+                    
+                     calculatedResult = calculatedResult * i;
+                    
+                }
+
+                Console.WriteLine(calculatedResult);
+            } else
+            {
+                Console.WriteLine("Määrittelemätön.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
