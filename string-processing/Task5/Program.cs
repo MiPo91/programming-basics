ae17d57a2e114b8979334b8293612ed6ebfedf60
MiPo91 <k4supur3@gmail.com>
2017-09-13 14:32:48 +0300

    Finished Task 5

---
+++ string-processing/Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give word!");
+            string userInput = Console.ReadLine();
+            string upperCase = userInput.ToUpper();
+
+            
+            char[] vowels = {'A', 'E', 'I', 'O', 'U', 'Y', 'Ä', 'Ö'}; // vowels to array
+            int vowelsCount = 0;
+            int wordLength = upperCase.Length;
+
+            // checking if characters match in word with vowels array
+            foreach(char vowel in vowels) {
+                for(int i = 0; i < wordLength; i++) {
+                    if(vowel == upperCase[i]) {
+                        vowelsCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Input: {0}, Output: Word {1} contains {2} vowels.", userInput, upperCase, vowelsCount);
+            Console.ReadKey();
+        }
+    }
+}
