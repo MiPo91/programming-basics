594609a5f8070c57b634bc221ef03b132fba2639
MiPo91 <k4supur3@gmail.com>
2017-09-13 09:28:26 +0300

    Finished Loops Tasks 4 and 5

---
+++ Loops/Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give number!");
+
+            string numberGiven = Console.ReadLine();
+            int convertedNumber;
+            int.TryParse(numberGiven, out convertedNumber);
+            int calculatedResult = 0;
+
+            if (convertedNumber > 0) { // calculating with positive numbers
+                for (int i = 1; i <= convertedNumber; i++)
+                {
+
+                    calculatedResult = calculatedResult + i;
+
+                }
+
+                Console.WriteLine(calculatedResult);
+            } else if(convertedNumber < 0) { // calculating with negative numbers
+                for (int i = -1; i >= convertedNumber; i--)
+                {
+                    calculatedResult = calculatedResult - i;
+                }
+
+                Console.WriteLine("-"+calculatedResult);
+            }
+            else
+            {
+                Console.WriteLine("Määrittelemätön.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loops/Task5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give number!");
+
+            string numberGiven = Console.ReadLine();
+            int convertedNumber;
+            int.TryParse(numberGiven, out convertedNumber);
+            int even = 0;
+            int odd = 0;
+
+            if (convertedNumber > 0) { //positive
+                for (int i = 1; i <= convertedNumber; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        even = even + i;
+                    }
+                    else
+                    {
+                        odd = odd + i;
+                    }
+
+                }
+
+                Console.WriteLine("Parittomien summa = {0}, Parillisten summa = {1}", odd, even);
+            } else if(convertedNumber < 0) { // negative 
+                for (int i = -1; i >= convertedNumber; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        even = even - i;
+                    }
+                    else
+                    {
+                        odd = odd - i;
+                    }
+
+                }
+
+                Console.WriteLine("Parittomien summa = -{0}, Parillisten summa = -{1}", odd, even);
+            }
+            else
+            {
+                Console.WriteLine("Määrittelemätön.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
