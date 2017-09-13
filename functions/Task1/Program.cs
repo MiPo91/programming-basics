cdca8cbcdd6e066ee0a53d0f60d3f5eb20a88a92
MiPo91 <k4supur3@gmail.com>
2017-09-13 11:21:24 +0300

    Finished Task 1

---
+++ functions/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give number!");
+
+            string input = Console.ReadLine();
+            int usersNumber;
+            int.TryParse(input, out usersNumber);
+
+            string functionResults = calculateStars(usersNumber); // calling function for results
+            Console.WriteLine(functionResults);
+
+            Console.ReadKey();
+        }
+
+        public static string calculateStars(int usersNumber)
+        {
+            string result = "";
+            if(usersNumber > 0) {
+                for(int i = 0; i < usersNumber; i++) { // summing stars to variable through loop
+                    result += "*";
+                }
+            } else {
+                result = "Number " + usersNumber + " is not allowed number.";
+            }
+
+            return result;
+        }
+    }
+}
