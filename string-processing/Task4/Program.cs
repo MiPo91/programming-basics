ccc73cfd2abf51ade9e87d88220a13956689e8b6
MiPo91 <k4supur3@gmail.com>
2017-09-13 14:13:38 +0300

    Finished Task 4

---
+++ string-processing/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give string!");
+            string userInput = Console.ReadLine().Replace(" ",""); //replace white space for word comparisong
+            string upperCase = userInput.ToUpper();
+
+            int arraySize = upperCase.Length - 1;
+
+            string wordBackwards = "";
+            
+            // turning word backwards
+            for(int i = arraySize; i >= 0; i--) {
+                wordBackwards += upperCase[i];
+            }
+
+            if(upperCase == wordBackwards) {
+                Console.WriteLine("Input: {0}, Output: {1} on palintromi.", userInput, upperCase);
+            } else {
+                Console.WriteLine("Input: {0}, Output: {1} ei ole palintromi.", userInput, upperCase);
+            }
+            Console.ReadKey();
+        }
+    }
+}
