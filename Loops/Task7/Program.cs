6122ab47ccd0ebff7da4de286024d748d599a188
MiPo91 <k4supur3@gmail.com>
2017-09-13 09:49:35 +0300

    Finished Loops Task 7

---
+++ Loops/Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            Console.WriteLine("Multiplication table for numbers 1-9.");
+
+            for(int i = 1; i < 10; i++) { // doing the calculation
+                number = i * i;
+                Console.WriteLine("{0} x {0} = {1}", i, number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
