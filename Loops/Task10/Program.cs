53e37ba08a0e9a18e1f576d31e045f17f82e7720
MiPo91 <k4supur3@gmail.com>
2017-09-13 10:44:55 +0300

    Finished Task 10

---
+++ Loops/Task10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 13; i++) {
+                double randomedNumber = rnd.NextDouble(); // generating random double number between 0 and 1
+                randomedNumber = Math.Round(randomedNumber, 1); // rounding it up to single decimal
+
+                if (randomedNumber <= 0.4) {
+                    Console.WriteLine("1");
+                }
+                else if (randomedNumber > 0.4 && randomedNumber <= 0.6) {
+                    Console.WriteLine("X");
+                }
+                else {
+                    Console.WriteLine("2");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
