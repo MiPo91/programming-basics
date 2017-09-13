b93be6e391b6038f4af9e14fbeeec7ad3429c373
MiPo91 <k4supur3@gmail.com>
2017-09-13 11:41:20 +0300

    Finished Task 2

---
+++ functions/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User inputs
+            Console.WriteLine("Give first Number");
+            string userInput1 = Console.ReadLine();
+            Console.WriteLine("Give second Number");
+            string userInput2 = Console.ReadLine();
+
+            // Parsing to int
+            int number1;
+            int number2;
+            int.TryParse(userInput1, out number1);
+            int.TryParse(userInput2, out number2);
+
+            // Calling function for results
+            string functionResult = Minimi(number1, number2);
+
+            // Displaying the results
+            Console.WriteLine(functionResult);
+            Console.ReadKey();
+        }
+
+        // Function to find smaller number
+        public static string Minimi(int first, int second) {
+            if(first < second) {
+                return ""+first;
+            } else if(first > second) {
+                return ""+second;
+            } else {
+                return "numbers are equal";
+            }
+        }
+
+
+       
+    }
+}
