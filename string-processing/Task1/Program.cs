55764ce52a18a2bdafcde639a5e2c17f05dcc7b6
MiPo91 <k4supur3@gmail.com>
2017-09-13 12:49:46 +0300

    Finished Task 1

---
+++ string-processing/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give string!");
+            string userInput = Console.ReadLine();
+            int strLength = userInput.Length; // getting length of sring
+
+            Console.WriteLine("Your inputted string has {0} characters", strLength);
+            Console.ReadKey();
+        }
+    }
+}
