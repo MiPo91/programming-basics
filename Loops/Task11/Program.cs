4de3d1b84bb3eb9b6799add3cc3654f7ff2a18b3
MiPo91 <k4supur3@gmail.com>
2017-09-13 10:53:55 +0300

    Finished Task 11

---
+++ Loops/Task11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int n6 = 0; // number of 6's counted
+
+            for (int i = 1; i <= 1000; i++) {
+                int dice = rnd.Next(1, 7);
+                if (dice == 6) {
+                    n6++;
+                }
+                Console.WriteLine("{0}. {1}", i, dice);
+            }
+            Console.WriteLine("Number six was randomed {0} number of times", n6);
+            Console.ReadKey();
+        }
+    }
+}
