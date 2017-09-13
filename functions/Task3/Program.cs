6b0afbc354c9f32d0068b16a5aaeff0a002d2575
MiPo91 <k4supur3@gmail.com>
2017-09-13 12:01:00 +0300

    Finished Task 3

---
+++ functions/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generating up and down limit for numbers
+            Random rng = new Random();
+            int downLimit = rng.Next(1, 15);
+            int upLimit = rng.Next(15, 30);
+
+            Console.WriteLine("Give number between {0} and {1}", downLimit, upLimit);
+
+            // Calling function for result
+            int usersNumber = pyyda_luku_valilta(downLimit, upLimit);
+
+            Console.WriteLine("Users given number is {0}",usersNumber);
+            Console.ReadKey();
+
+        }
+
+        public static int pyyda_luku_valilta(int downlimit, int uplimit)
+        {
+            int usersNumber;
+
+            while (true) // looping indefinetly until IF is true
+            {
+                string userInput = Console.ReadLine(); // users input
+                int.TryParse(userInput, out usersNumber);
+
+                if (usersNumber >= downlimit && usersNumber <= uplimit) { // Checking that input is between the values
+                    break;
+                } else {
+                    Console.WriteLine("Give number between {0} and {1}", downlimit, uplimit);
+                }
+            }
+
+            return usersNumber;
+
+        }
+    }
+}
