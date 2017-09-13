c9ea9f2d44bfd1e4b4050c30ab73f4d8ebe8d13e
MiPo91 <k4supur3@gmail.com>
2017-09-13 10:04:19 +0300

    Finished Task 8

---
+++ Loops/Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listing 20 random numbers between numbers 0 - 50");
+            int randomNumber;
+            Random rnd = new Random();
+            for (int i = 1; i <= 20; i++) // Looping 1-20
+            {
+                randomNumber =  rnd.Next(0, 50); // Generating random number between 0 and 50
+                Console.WriteLine("Random number {0} = {1}", i, randomNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
