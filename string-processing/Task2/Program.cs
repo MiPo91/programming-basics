c0de9779b710ae6178003595390608a07d82fcea
MiPo91 <k4supur3@gmail.com>
2017-09-13 12:54:01 +0300

    Finished Task 2

---
+++ string-processing/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give string!");
+            string userInput = Console.ReadLine();
+            string replacedText = userInput.Replace("e","@"); // replacing e with @
+
+            Console.WriteLine("Input: {0}, Output: {1}", userInput, replacedText);
+            Console.ReadKey();
+        }
+    }
+}
