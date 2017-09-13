1651358f54955dcf61da84a3561fe565d3279563
MiPo91 <k4supur3@gmail.com>
2017-09-13 09:43:24 +0300

    Finished Loops Task 6

---
+++ Loops/Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double neliojuuri = 0;
+            for(int i = 1;i <= 10;i++)
+            {
+                neliojuuri = Math.Sqrt(i);
+                neliojuuri = Math.Round(neliojuuri, 2);
+                Console.WriteLine("Number {0} square root is {1}", i, neliojuuri);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
