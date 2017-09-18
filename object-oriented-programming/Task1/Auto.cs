c1ba29c4130b5396bc2f474e65f54fa237e76434
MiPo91 <k4supur3@gmail.com>
2017-09-18 08:27:04 +0300

    Finished Task

---
+++ object-oriented-programming/Task1/Auto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Auto
+    {
+        public string merkki;
+        public double nopeus;
+
+        public Auto()
+        {
+            merkki = "Ford";
+            nopeus = 80;
+        }
+
+        public Auto(string Merkki, double Nopeus)
+        {
+            merkki = Merkki;
+            nopeus = Nopeus;
+        }
+
+        public string naytaTiedot()
+        {
+            Console.WriteLine("Merkki: {0}", merkki);
+            Console.WriteLine("Nopeus: {0} km/h", nopeus);
+            return merkki;
+        }
+
+        public double kiihdyta(double Nopeus)
+        {
+            nopeus = nopeus + Nopeus;
+
+            return nopeus;
+        }
+
+
+        public double jarruta()
+        {
+            nopeus = nopeus * 0.9;
+
+            return nopeus;
+        }
+    }
+}
---
+++ object-oriented-programming/Task1/Testeri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Testeri
+    {
+        static void Main(string[] args)
+        {
+            var auto1 = new Auto("BMW", 120);
+            auto1.kiihdyta(40);
+            auto1.jarruta();
+            auto1.naytaTiedot();
+
+            var auto2 = new Auto("Audi", 100);
+            auto2.jarruta();
+            auto2.naytaTiedot();
+
+            var auto3 = new Auto("Nissan", 90);
+            auto3.naytaTiedot();
+
+
+            Console.ReadKey();
+        }
+    }
+}
